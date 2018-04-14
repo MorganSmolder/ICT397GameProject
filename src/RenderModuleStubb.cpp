2f96a3776f06434f4739762b85a707bcd7a4ca7e
madmat333 <mathew.devene@gmail.com>
2018-04-15 00:50:02 +0800

    Add multi light handling

--- src/RenderModuleStubb.cpp
+++ src/RenderModuleStubb.cpp
@@ -1,12 +1,9 @@
 #include "RenderModuleStubb.h"
 
 RenderModuleStubb::RenderModuleStubb() {
-	id.setName("RM");
-	wireframe = false;
 }
 
 void RenderModuleStubb::DrawQuad(point tl, point br, float y) {
-	glDisable(GL_LIGHTING);
 	glBegin(GL_QUADS);
 	glTexCoord2f(0, 1);
 	glVertex3f(tl.x, tl.y, y);
@@ -17,7 +14,6 @@ void RenderModuleStubb::DrawQuad(point tl, point br, float y) {
 	glTexCoord2f(1, 1);
 	glVertex3f(br.x, tl.y, y);
 	glEnd();
-	glEnable(GL_LIGHTING);
 }
 
 void RenderModuleStubb::DrawQuad(vec3 tl, float widthx, float widthz, float height, vec3 trans){
@@ -174,7 +170,6 @@ void RenderModuleStubb::init(int argc, char** argv) {
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 	glfwWindowHint(GLFW_DEPTH_BITS, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
-
 	window = glfwCreateWindow(1280, 720, "Game Engine", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
@@ -204,47 +199,26 @@ void RenderModuleStubb::init(int argc, char** argv) {
 	glShadeModel(GL_SMOOTH);
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
 
-	float ambientLight[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-	float diffuseLight[] = { 1, 1, 1, 1.0f };
-	float specularLight[] = { 1, 1, 1, 1.0f };
-	float position[] = { 1.0f, 0.5f, 1.0f, 0 };
+
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	
+	//glEnable(GL_LIGHT0);
 	
 	glEnable(GL_COLOR_MATERIAL);
 	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specularLight);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, ambientLight);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, lights[0].specularLight);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, lights[0].ambientLight);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	// Assign created components to GL_LIGHT0
-	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
-	glLightfv(GL_LIGHT0, GL_POSITION, position);
-}
-
-void RenderModuleStubb::msgrcvr() {
-	MessagingBus* tmp = Singleton<MessagingBus>::getInstance();
-	Message tmpm;
+	configureLights();
+	commitLights();
 	
-	while (tmp->hasMessage(id)) {
-
-		tmpm = tmp->getMessage(id);
-
-		if (tmpm.getInstruction() == WIREFRAME) {
-			if (wireframe) {
-				glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-			}
-			else {
-				glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-			}
-			wireframe = !wireframe;
-		}
-	}
 }
+
 
 void RenderModuleStubb::startRenderCycle() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
@@ -261,6 +235,16 @@ void RenderModuleStubb::startRenderCycle() {
 void RenderModuleStubb::RenderFacingCamera() {
 	glPushMatrix();
 	glLoadIdentity();
+	glBegin(GL_QUADS);
+	glTexCoord2f(0, 1);
+	glVertex3f(0, 1, -1);
+	glTexCoord2f(0, 0);
+	glVertex3f(0, 0, -1);
+	glTexCoord2f(1, 0);
+	glVertex3f(1, 0, -1);
+	glTexCoord2f(1, 1);
+	glVertex3f(1, 1, -1);
+	glEnd();
 }
 
 void RenderModuleStubb::StopRenderFacingCamera() {
@@ -272,7 +256,6 @@ void RenderModuleStubb::endRenderCycle() {
 	glFlush();
 	glfwSwapBuffers(window);
 	glfwPollEvents();
-	msgrcvr();
 }
 
 
@@ -305,4 +288,134 @@ float RenderModuleStubb::getTimeSinceUpdate() {
 	float returnTime = (currentTime - timeLastUpdate);
 	timeLastUpdate = currentTime;
 	return returnTime;
-};
+}
+
+void RenderModuleStubb::configureLights() {
+	
+		glEnable(GL_LIGHTING);
+
+		//light 0
+		lights[0].enabled = true;
+		setLightdiffuse(0, 1, 1, 1, 1.0f);
+		setLightAmbient(0, 0.0f, 0.0f, 0.0f, 1.0f);
+		setLightspecular(0, 1, 1, 1, 1.0f);
+		setLightposition(0, 1.0f, 0.5f, 1.0f, 0);
+	
+			
+
+	for (int i = 0; i < 8; i++) {
+		if (lights[i].enabled) {
+			switch (i) {
+			case 0: glEnable(GL_LIGHT0);
+				break;
+			case 1: glEnable(GL_LIGHT1);
+				break;
+			case 2: glEnable(GL_LIGHT2);
+				break;
+			case 3: glEnable(GL_LIGHT3);
+				break;
+			case 4: glEnable(GL_LIGHT4);
+				break;
+			case 5: glEnable(GL_LIGHT5);
+				break;
+			case 6: glEnable(GL_LIGHT6);
+				break;
+			case 7: glEnable(GL_LIGHT7);
+				break;
+			}
+		}
+	}
+
+}
+
+void RenderModuleStubb::commitLights() {
+	for (int i = 0; i < 8; i++) {
+		if (lights[i].enabled) {
+			switch (i) {
+			case 0: {		glLightfv(GL_LIGHT0, GL_AMBIENT, lights[0].ambientLight);
+							glLightfv(GL_LIGHT0, GL_DIFFUSE, lights[0].diffuseLight);
+							glLightfv(GL_LIGHT0, GL_SPECULAR, lights[0].specularLight);
+							glLightfv(GL_LIGHT0, GL_POSITION, lights[0].position);
+							break;
+			}
+			case 1: {		glLightfv(GL_LIGHT1, GL_AMBIENT, lights[1].ambientLight);
+							glLightfv(GL_LIGHT1, GL_DIFFUSE, lights[1].diffuseLight);
+							glLightfv(GL_LIGHT1, GL_SPECULAR, lights[1].specularLight);
+							glLightfv(GL_LIGHT1, GL_POSITION, lights[1].position);
+							break;
+			}
+			case 2: {		glLightfv(GL_LIGHT2, GL_AMBIENT, lights[2].ambientLight);
+							glLightfv(GL_LIGHT2, GL_DIFFUSE, lights[2].diffuseLight);
+							glLightfv(GL_LIGHT2, GL_SPECULAR, lights[2].specularLight);
+							glLightfv(GL_LIGHT2, GL_POSITION, lights[2].position);
+							break;
+			}
+			case 3: {		glLightfv(GL_LIGHT3, GL_AMBIENT, lights[3].ambientLight);
+							glLightfv(GL_LIGHT3, GL_DIFFUSE, lights[3].diffuseLight);
+							glLightfv(GL_LIGHT3, GL_SPECULAR, lights[3].specularLight);
+							glLightfv(GL_LIGHT3, GL_POSITION, lights[3].position);
+							break;
+			}
+			case 4: {		glLightfv(GL_LIGHT4, GL_AMBIENT, lights[4].ambientLight);
+							glLightfv(GL_LIGHT4, GL_DIFFUSE, lights[4].diffuseLight);
+							glLightfv(GL_LIGHT4, GL_SPECULAR, lights[4].specularLight);
+							glLightfv(GL_LIGHT4, GL_POSITION, lights[4].position);
+							break;
+			}
+			case 5: {		glLightfv(GL_LIGHT5, GL_AMBIENT, lights[5].ambientLight);
+							glLightfv(GL_LIGHT5, GL_DIFFUSE, lights[5].diffuseLight);
+							glLightfv(GL_LIGHT5, GL_SPECULAR, lights[5].specularLight);
+							glLightfv(GL_LIGHT5, GL_POSITION, lights[5].position);
+							break;
+			}
+			case 6: {		glLightfv(GL_LIGHT6, GL_AMBIENT, lights[6].ambientLight);
+							glLightfv(GL_LIGHT6, GL_DIFFUSE, lights[6].diffuseLight);
+							glLightfv(GL_LIGHT6, GL_SPECULAR, lights[6].specularLight);
+							glLightfv(GL_LIGHT6, GL_POSITION, lights[6].position);
+							break;
+			}
+			case 7: {		glLightfv(GL_LIGHT7, GL_AMBIENT, lights[7].ambientLight);
+							glLightfv(GL_LIGHT7, GL_DIFFUSE, lights[7].diffuseLight);
+							glLightfv(GL_LIGHT7, GL_SPECULAR, lights[7].specularLight);
+							glLightfv(GL_LIGHT7, GL_POSITION, lights[7].position);
+							break;
+			}
+			}
+		}
+	}
+}
+
+void RenderModuleStubb::setLightAmbient(int lightNo, float first, float second, float third, float fourth) {
+		if (lightNo < 8 && lightNo > -1) {
+			lights[lightNo].ambientLight[0] = first;
+			lights[lightNo].ambientLight[1] = second;
+			lights[lightNo].ambientLight[2] = third;
+			lights[lightNo].ambientLight[3] = fourth;
+		}
+}
+	void RenderModuleStubb::setLightdiffuse(int lightNo, float first, float second, float third, float fourth) {
+		if (lightNo < 8 && lightNo > -1) {
+			lights[lightNo].diffuseLight[0] = first;
+			lights[lightNo].diffuseLight[1] = second;
+			lights[lightNo].diffuseLight[2] = third;
+			lights[lightNo].diffuseLight[3] = fourth;
+		}
+	};
+	void RenderModuleStubb::setLightspecular(int lightNo, float first, float second, float third, float fourth) {
+		if (lightNo < 8 && lightNo > -1) {
+			lights[lightNo].specularLight[0] = first;
+			lights[lightNo].specularLight[1] = second;
+			lights[lightNo].specularLight[2] = third;
+			lights[lightNo].specularLight[3] = fourth;
+		}
+	};
+	void RenderModuleStubb::setLightposition(int lightNo, float first, float second, float third, float fourth) {
+		if (lightNo < 8 && lightNo > -1) {
+			lights[lightNo].position[0] = first;
+			lights[lightNo].position[1] = second;
+			lights[lightNo].position[2] = third;
+			lights[lightNo].position[3] = fourth;
+		}
+
+	};
+
